9d8aca3a01d064ff5f833a58dc484fde6808382e
ImR0Ns <dariusrosu11@yahoo.com>
2021-08-19 17:30:18 +0300

    final updates

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 // alert(); (test)
 
-var secretWord = "Secret"; // change secret word
+var secretWord = "secret"; // change secret word
 function secretWordToWhite(word){
-    var whiteSpaces = "", len = secretWord.length - 2;
+    var whiteSpaces = "";
     var start = word[0], end = word[word.length - 1];
-    whiteSpaces = start + "_".repeat(len) + end;
+    for(var k in secretWord){
+        if(secretWord[k].toLowerCase() === start.toLowerCase()){
+            whiteSpaces += start;
+        } else if(secretWord[k].toLowerCase() === end.toLowerCase()){
+            whiteSpaces += end;
+        } else {
+            whiteSpaces += "_"
+        }
+    }
     return whiteSpaces;
 }
 
@@ -17,7 +25,7 @@ $(document).ready(function(){
     $("input").on("keydown",function search(e) {
         if(e.keyCode == 13) {
             var userInput = $(this).val();
-            if(forCheck.join("") == white.toLowerCase()){
+            if(!white.includes("_")){
                 alert("You won!");
                 location.reload(); // restart game;
             }else if(wordsUsed.includes(userInput)){
@@ -33,12 +41,34 @@ $(document).ready(function(){
                 white = splitTwo.join("");
                 $(".secretWord").text(white);
             } else {
-                if(mistakes > 4){
+                var op = mistakes + 1;
+                if(op > 6){
                     alert("You lose!");
                     location.reload(); // restart game;
                 } else {
                     mistakes += 1;
                     $(".mistakes").text("Mistakes: " + mistakes);
+                    switch (mistakes) {
+                        case 1:
+                            $("#mist").attr("src","/imgs/1mist.png");
+                            break;
+                        case 2:
+                            $("#mist").attr("src","/imgs/2mist.png");
+                            break;
+                        case 3:
+                            $("#mist").attr("src","/imgs/3mist.png");
+                            break;
+                        case 4:
+                            $("#mist").attr("src","/imgs/4mist.png");
+                            break;
+                        case 5:
+                            $("#mist").attr("src","/imgs/5mist.png");
+                            break;
+                        case 6:
+                            $("#mist").attr("src","/imgs/6mist.png");
+                            $(".mistakes").text("Last chance!");
+                            break;
+                    }
                 }
             }
         }
